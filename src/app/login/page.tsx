39a11f729855ae018db74fc9c9c68685309fd0b6
Igor Henrique Ramon <igorhenriqueramon@gmail.com>
2025-12-01 03:11:37 +0000

    apenas diminua um pouco a altura da página, assim o footer irá subir e n

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -89,7 +89,7 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="h-screen w-full lg:grid lg:grid-cols-2">
+    <div className="w-full lg:grid lg:grid-cols-2">
        <div className="hidden bg-gradient-to-br from-primary to-purple-600 lg:flex flex-col items-center justify-center p-12 text-center relative overflow-hidden">
           <motion.div 
             initial={{ opacity: 0, y: -20 }}
