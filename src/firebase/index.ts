b0b8950d38dbf4fdc4f84b30f392d571bd8d4a88
Igor Henrique Ramon <igorhenriqueramon@gmail.com>
2025-12-01 17:23:29 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,16 +3,7 @@
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getFirestore, type Firestore } from "firebase/firestore";
 import { getAuth, type Auth } from "firebase/auth";
-
-// As this file is a client component, we can use env vars
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+import { firebaseConfig } from "./config";
 
 let app: FirebaseApp;
 let auth: Auth;
