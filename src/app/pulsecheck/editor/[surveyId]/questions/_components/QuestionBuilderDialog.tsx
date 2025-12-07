eb7da2003a68e1416a4f799f424d8065f192b5b9
Igor Henrique Ramon <igorhenriqueramon@gmail.com>
2025-12-07 19:40:15 +0000

    Try fixing this error: `Runtime Error: Error: Label is not defined. Erro

--- src/app/pulsecheck/editor/[surveyId]/questions/_components/QuestionBuilderDialog.tsx
+++ src/app/pulsecheck/editor/[surveyId]/questions/_components/QuestionBuilderDialog.tsx
@@ -10,6 +10,7 @@ import { cn } from '@/lib/utils';
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from '@/components/ui/dialog';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Button } from '@/components/ui/button';
